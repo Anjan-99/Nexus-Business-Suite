a3db6b451c19f0e6ad3ad48f6789eb43bf43ce5b
Anjan_knowns <al@titanslab.in>
2023-09-22 12:49:25 +0530

    DB Changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema ({
     name: String,
     email: String,
     password: String,
+})
+
+userSchema.pre("save", async function(next){
+    console.log("testing");
+    if(this.isModified("password")){
+        this.password = bcrypt.hash(this.password, 12);
+    }
+    next();
 })
 
 const User = mongoose.model("USER", userSchema,"Users");
