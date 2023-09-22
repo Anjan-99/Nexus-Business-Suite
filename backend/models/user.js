e4bed7a85deb076f51f4b48f5d5a841c75567c30
Anjan_knowns <al@titanslab.in>
2023-09-22 13:01:52 +0530

    Revert "DB Changes"
    This reverts commit a3db6b451c19f0e6ad3ad48f6789eb43bf43ce5b.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,18 +1,9 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema ({
     name: String,
     email: String,
     password: String,
-})
-
-userSchema.pre("save", async function(next){
-    console.log("testing");
-    if(this.isModified("password")){
-        this.password = bcrypt.hash(this.password, 12);
-    }
-    next();
 })
 
 const User = mongoose.model("USER", userSchema,"Users");
--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router();
 require("../db/conn.js");
-const mongoose = require("mongoose");
-const mongodb = require("mongodb");
 const User = require("../models/user.js");
 // fOR ROUTER TESTING PURPOSE
 // router.get("/", (req, res) => {
 //   res.send("Hello World! from router");
 // });
-
-//Router
-//Register
 router.post("/user", async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
@@ -22,9 +17,7 @@ router.post("/user", async (req, res) => {
             return res.status(422).json({ error: "Email already exist" });
         }
         const user = new User({ name, email, password });
-        
-        await user.save();
-        console.log(userreg);
+        const userreg = await user.save();
         if (userreg){
             res.status(201).json({ message: "User registered successfully" });
         } else {
@@ -34,32 +27,6 @@ router.post("/user", async (req, res) => {
         console.log(err);
     }
 });
-
-//login 
-router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password){
-            return res.status(400).json({ error: "Please fill all the fields" });
-        }
-        const userlogin = await User.findOne({ email: email });
-        //console.log(userlogin); for testing
-        if (userlogin) {
-            const isMatch = await userlogin.password === password;
-            if (isMatch) {
-                res.status(201).json({ message: "User logged in successfully" });
-            } else {
-                res.status(500).json({ error: "Invalid Credentials" });
-            }
-        }
-    }
-    catch (err) {
-        console.log(err);
-    }
-});
-
-
-
     //console.log(req.body); // to print data in console for testing
     //if function is not working
     //res.json({message : req.body}); to print data in console for testing
