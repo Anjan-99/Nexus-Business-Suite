03adffab84d114a2ffc2df735097b58f2a78a6e9
Anjan_knowns <al@titanslab.in>
2023-09-27 10:53:29 +0530

    JWT

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -3,8 +3,11 @@ const router = express.Router();
 require("../db/conn.js");
 const User = require("../models/user.js");
 const bcrypt = require("bcryptjs");
+<<<<<<< HEAD
 const jwt = require("jsonwebtoken");
 const authenticate = require("../middleware/authentication");
+=======
+>>>>>>> parent of da0b2ce (JBT)
 
 //Router
 //Register
@@ -41,6 +44,7 @@ router.post("/login", async (req, res) => {
         
         if (userLogin){
             const isMatch = await bcrypt.compare(password, userLogin.password);
+<<<<<<< HEAD
             const token = await userLogin.generateAuthToken();
             console.log(token);
 
@@ -49,6 +53,8 @@ router.post("/login", async (req, res) => {
                 httpOnly:true
             });
 
+=======
+>>>>>>> parent of da0b2ce (JBT)
             if (isMatch){
                 res.status(201).json({ message: "User logged in successfully" });
             } else {
--- frontend/src/pages/auth/common/login-form.jsx
+++ frontend/src/pages/auth/common/login-form.jsx
@@ -9,6 +9,8 @@ import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { handleLogin } from "./store";
 import { toast } from "react-toastify";
+import axios from "axios";
+
 const schema = yup
   .object({
     email: yup.string().email("Invalid email").required("Email is Required"),
@@ -16,23 +18,18 @@ const schema = yup
   })
   .required();
 const LoginForm = () => {
-  const dispatch = useDispatch();
-  const { users } = useSelector((state) => state.auth);
   const {
     register,
     formState: { errors },
-    handleSubmit,
   } = useForm({
     resolver: yupResolver(schema),
     //
     mode: "all",
   });
   const navigate = useNavigate();
-<<<<<<< HEAD
-
 
   const [user , setUser] = useState({email:'',password:''})
-  const handleSubmit = (e) => {
+  const submit = (e) => {
     const { name, value } = e.target;
     setUser({...user,[name]:value})
   }
@@ -40,69 +37,45 @@ const LoginForm = () => {
   const LoginUser = async (e) => {
     e.preventDefault();
     const { email, password } = user;
+    console.log(email, password);
     try {
       const res = await axios.post("http://localhost:5000/login", {
         email,
         password,
-      }, { withCredentials: true });
+      });
       console.log(res);
       if (res.data.error) {
         alert(res.data.error);
       } else {
         alert(res.data.message);
-=======
-  const onSubmit = (data) => {
-    const user = users.find(
-      (user) => user.email === data.email && user.password === data.password
-    );
-    if (user) {
-      dispatch(handleLogin(true));
-      setTimeout(() => {
->>>>>>> parent of e07e0e9 (Login Connected)
         navigate("/dashboard");
-      }, 1500);
-    } else {
-      toast.error("Invalid credentials", {
-        position: "top-right",
-        autoClose: 1500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  const [checked, setChecked] = useState(false);
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 ">
+    <form className="space-y-4 ">
       <Textinput
         name="email"
         label="email"
-        defaultValue={users[0].email}
         type="email"
         register={register}
         error={errors.email}
+        onChange={submit}
         className="h-[48px]"
       />
       <Textinput
         name="password"
         label="passwrod"
         type="password"
-        defaultValue={users[0].password}
         register={register}
         error={errors.password}
+        onChange={submit}
         className="h-[48px]"
       />
       <div className="flex justify-between">
-        <Checkbox
-          value={checked}
-          onChange={() => setChecked(!checked)}
-          label="Keep me signed in"
-        />
         <Link
           to="/forgot-password"
           className="text-sm text-slate-800 dark:text-slate-400 leading-6 font-medium"
@@ -111,7 +84,7 @@ const LoginForm = () => {
         </Link>
       </div>
 
-      <button className="btn btn-dark block w-full text-center">Sign in</button>
+      <button className="btn btn-dark block w-full text-center" onClick={LoginUser}>Sign in</button>
     </form>
   );
 };
