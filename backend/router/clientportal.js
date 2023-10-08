aafebd82767b48c2a3c0a4f7bab4ca631fc04021
viralgautami <d22dcs141@charusat.edu.in>
2023-10-08 18:11:58 +0530

    client portal database

---
+++ backend/router/clientportal.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+require("../db/conn.js");
+const User = require("./clientportal.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+router.post("/clientportal", async (req, res) => {
+    const { portalid, customerid, portalurl } = req.body;
+    try { 
+        const clientportal = new Clientportal({ portalid, customerid, portalurl });
+        const clientportaldetails =  await clientportal.save();
+        if (clientportaldetails){
+            res.status(201).json({ message: "successfully" });
+        } else {
+            res.status(400).json({ message: "unsuccessfully" });
+        }
+    } catch (err){
+        console.log(err);
+    }
+});
