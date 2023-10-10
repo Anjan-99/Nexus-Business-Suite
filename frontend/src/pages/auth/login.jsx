737c3ffc53e3ea832fe49193938701d7534a9585
viralgautami <d22dcs141@charusat.edu.in>
2023-10-11 03:25:19 +0530

    login changes

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -23,8 +23,8 @@ const login = () => {
                 <img src={isDark ? LogoWhite : Logo} alt="" width="40%" className="mb-10" />
               </Link>
               <h4>
-              Business <br/>              <span className="text-slate-800 dark:text-slate-400 font-bold">
-              Powerhouse
+              A BUSINESS <br/>              <span className="text-slate-800 dark:text-slate-400 font-bold">
+              POWERHOUSE
                 </span>
               </h4>
             </div>
