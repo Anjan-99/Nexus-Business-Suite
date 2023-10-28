483922d8ebcea8f75f63c3f4c9a2363b931c4c7d
Anjan_knowns <al@titanslab.in>
2023-10-28 14:29:29 +0530

    vendor

--- frontend/src/pages/client/vendor_add.jsx
+++ frontend/src/pages/client/vendor_add.jsx
@@ -10,48 +10,32 @@ import * as yup from "yup";
 import CustAdd from "./react-tables/CustAdd";
 import axios from "axios";
 
-const schema = yup
-  .object({
-    email: yup.string().email("Invalid email").required("Email is Required"),
-    password: yup.string().required("Password is Required"),
-  })
-  .required();
 const vendoradd = () => {
-
-  const {
-    register,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-    //
-    mode: "all",
-  });
   const navigate = useNavigate();
-
-  const [cust , setUser] = useState()
-  const submit = (e) => {
-    const { name, value } = e.target;
-    setUser({...cust,[name]:value})
-  }
 
   const custadd = async (e) => {
     e.preventDefault();
-    
-    const { customer_name, invoice_number, mode_of_payment, date, amount, unused_amount} = cust; //form inputs
+    const name = document.getElementById("name").value;
+    const company_name = document.getElementById("company_name").value;
+    const email = document.getElementById("email").value;
+    const payable_amount = document.getElementById("payable_amount").value;
+    const unused_credit = document.getElementById("unused_credit").value;
     try {
-      const res = await axios.post("http://localhost:5000/customer_add", {
-        name,
-        company_name,
-        email,
-        payable_amount,
-        unused_credit
-      },{ withCredentials: true });
+      const res = await axios.post(
+        "http://localhost:5000/vendor_add",
+        {
+          name,
+          company_name,
+          email,
+          payable_amount,
+          unused_credit,
+        },
+        { withCredentials: true }
+      );
       console.log(res);
       if (res) {
         alert(res.data.message);
-        setTimeout(() => {
-          navigate("/vendor_table"); //redirect to table
-        }, 1500);
+        navigate("/vendortable");
       } else {
         alert(res.data.error);
       }
@@ -62,58 +46,45 @@ const vendoradd = () => {
 
   return (
     <div>
-      <Card title="Add Vendor"> 
+      <Card title="Add Vendor">
         <div>
           <form className="space-y-4 ">
             <div className="grid xl:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-5">
               <Textinput
-                name = "name"
                 label="Name"
-                register={register}
-                onChange={submit}
-                isMask
                 placeholder="Ex: viral gautami, ... etc."
+                id="name"
+                isMask
               />
               <Textinput
-                name = "company_name"
                 label="Company Name"
-                register={register}
-                onChange={submit}
-                isMask
                 placeholder="Ex: Titanslab, etc."
+                id="company_name"
+                isMask
               />
               <Textinput
-                name = "email"
                 label="Email"
-                register={register}
-                onChange={submit}
-                isMask
                 placeholder="Ex: viral@gmail.com,... etc"
+                id="email"
+                isMask
               />
               <Textinput
-                name = "payable_amount"
                 label="Payable Amount"
-                register={register}
-                onChange={submit}
-                isMask
                 placeholder="Ex: $5000,.. etc."
-              />
-
-              <Textinput
-                name = "unused_credit"
-                label="Unused Credit"
-                register={register}
-                onChange={submit}
-                placeholder="$8000"
+                id="payable_amount"
                 isMask
               />
             </div>
-            <div className="grid xl:grid-cols-1 grid-cols-1 gap-5">
-              <Textarea label="Address" id="pn4" name="address" placeholder="Address" register={register}
-                onChange={submit} />
-            </div>
+            <Textinput
+              label="Unused Credit"
+              placeholder="$8000"
+              id="unused_credit"
+              isMask
+            />
             <div className="ltr:text-left rtl:text-right">
-              <button className="btn btn-primary text-center" onClick={custadd}>Submit</button>
+              <button className="btn btn-primary text-center" onClick={custadd}>
+                Submit
+              </button>
             </div>
           </form>
         </div>
@@ -121,11 +92,6 @@ const vendoradd = () => {
       </Card>
     </div>
   );
-  
 };
 
-
 export default vendoradd;
-
-
-
